e413b2c1973df8f0a65d9af290ae5736cae929da
Tuan Anh <tuananh223355@gmail.com>
2026-04-14 02:35:58 +0700

    FIX

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('monitor:log')->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
